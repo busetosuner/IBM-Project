e4569eba17d08c6c5603d841cb81b5ed9b73ff28
Buse Tosuner <busetosuner@gmail.com>
2023-07-13 16:26:29 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 import sys
 
+
+#Hangi data formatında kontrol ediyor
 def check_data_format(file_path):
     _, file_extension = os.path.splitext(file_path)
     
@@ -16,17 +18,85 @@ def check_data_format(file_path):
 
 file_path = 'DataScienceSalaries.csv'  
 
-data_format = check_data_format(file_path)
+df = pd.read_csv('DataScienceSalaries.csv')
 
+data_format = check_data_format(file_path)
+# Eğer 3 data formatından biri değilse bitirecek
 if data_format is None:
     print("Error")
     sys.exit()
-else:
+else: # Burayı yazdırıyor burayla daha bir şey yaparız diye düşündüm ihtiyaç olursa
     print("Dataset:", data_format)
+
+# Eğer duplicate varsa sayması için
+def count_duplicates(df):
+    # Satırda yinelenen varsa
+    duplicated_rows = df.duplicated()
+    num_duplicates_rows = duplicated_rows.sum()
+
+    # Sütunda yinelenen varsa
+    duplicated_columns = df.T.duplicated()
+    num_duplicates_columns = duplicated_columns.sum()
+
+    # Sütunlar arasında yinelenen satırlar varsa
+    duplicated_rows_among_columns = df.duplicated(keep=False)
+    num_duplicates_rows_among_columns = duplicated_rows_among_columns.sum()
+
+    # Tamamen aynı olan satırlar varsa
+    duplicated_rows_all_columns = df.duplicated()
+    num_duplicates_rows_all_columns = duplicated_rows_all_columns.sum()
+
+    return num_duplicates_rows, num_duplicates_columns, num_duplicates_rows_among_columns, num_duplicates_rows_all_columns
+
+num_duplicates_rows, num_duplicates_columns, num_duplicates_rows_among_columns, num_duplicates_rows_all_columns = count_duplicates(df)
+
+# Toplam duplicate sayısı hesaplama
+total_duplicates = num_duplicates_rows + num_duplicates_columns + num_duplicates_rows_among_columns + num_duplicates_rows_all_columns
+
+print("Duplicate number:", total_duplicates)
+
+
+
+
+# Burası da temizleme kısmı
+def clean_duplicates(df):
+    # Satır düzeyinde yinelenen varsa
+    duplicated_rows = df.duplicated()
+
+    # Sütun düzeyinde yinelenen varsa
+    duplicated_columns = df.T.duplicated()
+
+    # Sütunlar arasında yinelenen varsa
+    duplicated_rows_among_columns = df.duplicated(keep=False)
+
+    # Tamamen aynı olan satırlar varsa
+    duplicated_rows_all_columns = df.duplicated()
+
+    # Yinelenen değerleri düzeltme fonksiyonu
+    df_cleaned = df.drop_duplicates()
+
+    return df_cleaned
+
+
+df_cleaned= clean_duplicates(df)
+
+
+# Temizlenmiş DataFrame'i kontrol etme
+print(df_cleaned.shape)  # DataFrame'in boyutunu yazdırma
+
+# Temizlendikten sonra sorun var mı check'i
+total_duplicates_after_cleaning = count_duplicates(df_cleaned)
+
+print("Duplicate number after cleaning:", sum(total_duplicates_after_cleaning))
     
     
     
+  
     
+    
+    
+   
+
     
     
     
