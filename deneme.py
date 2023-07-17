834479d135f3e6fe471305e2bc55010fbf45202f
Buse Tosuner <busetosuner@gmail.com>
2023-07-17 14:25:11 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -10,19 +10,32 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
 def check_data_format(file_path):
     _, file_extension = os.path.splitext(file_path)
     
+    # Creating empty dataFrame
+    df = pd.DataFrame()
+
+    data_format = ""
     if file_extension == '.csv':
-        return 'csv'
+        df = pd.read_csv(file_path)
+        data_format = 'csv'
     elif file_extension == '.xlsx' or file_extension == '.xls':
-        return 'excel'
+        df = pd.read_excel(file_path)
+        data_format = 'excel'
     elif file_extension == '.json':
-        return 'json'
+        df = pd.read_json(file_path)
+        data_format = 'json'
     else:
-        return None
+        print("Error")
+        sys.exit()
+
+    print("Dataset:", data_format)
+    return df
 
 file_path = 'DataScienceSalaries.csv'  
 
 df = pd.read_csv(file_path)
 data_format = check_data_format(file_path)
+
+
 # Eğer 3 data formatından biri değilse bitirecek
 if data_format is None:
     print("Error")
@@ -144,12 +157,6 @@ print(df_normalized.head())
 
 print("Standardized Data:")
 print(df_standardized.head())
-
-
-
-
-
-
 
 
 
