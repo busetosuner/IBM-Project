202a67acf601f374746b7d50adb72d3fb73fa3ba
Buse Tosuner <busetosuner@gmail.com>
2023-07-17 11:15:03 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
 import sys
+import numpy as np
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
+ # pip install scikit-learn yapmalısın
 
 
 #Hangi data formatında kontrol ediyor
@@ -18,8 +21,7 @@ def check_data_format(file_path):
 
 file_path = 'DataScienceSalaries.csv'  
 
-df = pd.read_csv('DataScienceSalaries.csv')
-
+df = pd.read_csv(file_path)
 data_format = check_data_format(file_path)
 # Eğer 3 data formatından biri değilse bitirecek
 if data_format is None:
@@ -28,6 +30,8 @@ if data_format is None:
 else: # Burayı yazdırıyor burayla daha bir şey yaparız diye düşündüm ihtiyaç olursa
     print("Dataset:", data_format)
 
+
+# DUPLICATE BOLUMU BASLANGICI
 # Eğer duplicate varsa sayması için
 def count_duplicates(df):
     # Satırda yinelenen varsa
@@ -90,19 +94,71 @@ total_duplicates_after_cleaning = count_duplicates(df_cleaned)
 print("Duplicate number after cleaning:", sum(total_duplicates_after_cleaning))
     
     
-    
-  
-    
-    
-    
-   
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
+# Categorical ve Numerical Data'yı ayırma bölümü
+  
+# Numerical data ile başlıyorum
+
+# Öncelikle outlierlari belirliyorum
+
+
+numeric_columns = df_cleaned.select_dtypes(include=np.number).columns
+
+# Numerik veri kümesini almak
+numeric_data = df_cleaned[numeric_columns]
+
+# Outlier tespiti
+def detect_outliers(data, threshold=3):
+    outliers = []
+    for column in data.columns:
+        # Z-puanı için
+        z_scores = (data[column] - data[column].mean()) / data[column].std()
+        # Z-puanının threshold değerinden büyük olanları aykırı değer olarak işaretle
+        outliers.extend(data[column][np.abs(z_scores) > threshold])
+    return outliers
+
+outliers = detect_outliers(numeric_data)
+
+# Outlierları çıkarmak için
+df_cleaned_no_outliers = df_cleaned.drop(pd.DataFrame(outliers).reset_index(drop=True))
+
+# Outlierların veri setindeki yüzdesini hesaplamak (outlierların veri setine etkisini görmek için)
+outlier_percentage = len(outliers) / len(df_cleaned) * 100
+
+
+print("Outliers:", outliers)
+#print("Veri Seti Sütunlari:", df_cleaned.columns)
+
+# Normalization
+scaler = MinMaxScaler()
+normalized_data = scaler.fit_transform(df_cleaned_no_outliers[numeric_columns])
+df_normalized = pd.DataFrame(normalized_data, columns=numeric_columns)
+
+# Standardization
+scaler = StandardScaler()
+standardized_data = scaler.fit_transform(df_cleaned_no_outliers[numeric_columns])
+df_standardized = pd.DataFrame(standardized_data, columns=numeric_columns)
+
+print("Normalized Data:")
+print(df_normalized.head())
+
+print("Standardized Data:")
+print(df_standardized.head())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
