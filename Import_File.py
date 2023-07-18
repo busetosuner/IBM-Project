631e3fcb1b813e1dfb496ffc6ed4408ab6b73eeb
hacerakinci <108008177+hacerakinci@users.noreply.github.com>
2023-07-18 16:22:56 +0300

    Update Import_File.py

--- Import_File.py
+++ Import_File.py
@@ -1,10 +1,7 @@
 import os
 import pandas as pd
 import sys
-import numpy as np
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
- # pip install scikit-learn yapmalısın
-
+import shutil
 
 #Hangi data formatında kontrol ediyor
 def check_data_format(file_path):
@@ -29,16 +26,3 @@ def check_data_format(file_path):
 
     print("Dataset:", data_format)
     return df
-
-file_path = 'DataScienceSalaries.csv'  
-
-df = pd.read_csv(file_path)
-data_format = check_data_format(file_path)
-
-
-# Eğer 3 data formatından biri değilse bitirecek
-if data_format is None:
-    print("Error")
-    sys.exit()
-else: # Burayı yazdırıyor burayla daha bir şey yaparız diye düşündüm ihtiyaç olursa
-    print("Dataset:", data_format)
