0f7baab282df375b96918bba60279d801bb1fc61
hacerakinci <108008177+hacerakinci@users.noreply.github.com>
2023-07-19 11:34:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import Import_File
 import handle_missing_values
 import Duplicates
 import numpy as np
+import dummy_variables
+import Binning
 
 # Path will be given by user
 file_path = 'C:\\Users\\hacer\\OneDrive\\Masaüstü\\IBM\\datasets\\'
@@ -19,6 +21,7 @@ headers = df.columns.values
 for i, header in enumerate(headers):
     print(i," ", header)
 
+# Inputs: target = 7, attributes = 2 8 11
 target = headers[int(input("Please enter index of target attribute: "))]
 group_list = [headers[int(item)] for item in input("Please enter the index of attributes you want: ").split()]
 
@@ -26,4 +29,26 @@ df = handle_missing_values.clean_missing(df, target)
 
 df = Duplicates.clean_duplicates(df)
 
-print(df.head())
+# Create  dictionary that keeps attribute names, while addingg dummy columns or creating bins update dictionary to get this new added columns
+mp = {}
+
+for header in headers:
+    mp[header] = header
+
+# After this point we get header names using dictionary
+ 
+for attribute in group_list:
+    if (df[attribute].nunique() <= 5):
+        # Pass numerical variables for sake of simplicity 
+        if (Binning.is_numeric(df[attribute])):
+            continue
+
+        df, mp[attribute] = dummy_variables.create_dummies(df, attribute)
+    else:
+        df, mp[attribute] = Binning.make_bins(df, attribute)
+    print("\nNew columns of",attribute,":",mp[attribute])
+
+print("Check if the dictionary is working:" )
+print("\n",df[mp["company_size"]].head())
+print("\n New columns: ", df.columns.values)
+print("\n",df.head())
