825a3b79996e18716def3b6e65e9d339cf3877b9
hacerakinci <108008177+hacerakinci@users.noreply.github.com>
2023-07-19 11:35:09 +0300

    Update dummy_variables.py

--- dummy_variables.py
+++ dummy_variables.py
@@ -1,39 +1,20 @@
 import pandas as pd
 import numpy as np
 
-# Import Data Frame
-
-file_path = r"C:\Users\hacer\OneDrive\Masaüstü\IBM\datasets\df.csv"
-
-df = pd.read_csv(file_path)
-
-# Get headers
-headers = df.columns.values
-
 # Dummy variables 
 
-# Keep original file, create new one
-df_new = df
+# if there are equal or less than 3 unique values turn into dummy variables
+def create_dummies(df, header):
+    # Create new dataframe for dummy variables
+    dummy_variable = pd.get_dummies(df[header], prefix=header)
 
-pd.options.mode.chained_assignment = None
+    # Turn True/False into 1/0 
+    dummy_variable = dummy_variable.astype(int)
 
-# for each column if there are equal or less than 3 unşque values turn into dummy variables
-for header in headers:
-    if (df[header].nunique() <= 3):
+    # Add to main dataframe
+    df = pd.concat([df, dummy_variable], axis=1)
 
-        # Create new dataframe for dummy variables
-        dummy_variable = pd.get_dummies(df[header], prefix=header)
+    # Drop the original column
+    df.drop(header, axis=1, inplace=True)
 
-        # Turn True/False into 1/0 
-        dummy_variable = dummy_variable.astype(int)
-
-        # Add to main dataframe
-        df_new = pd.concat([df_new, dummy_variable], axis=1)
-
-        # Drop the original column
-        df_new.drop(header, axis=1, inplace=True)
-
-# Export the new file -this lines will be deleted after other steps is completed-
-
-path = 'C:\\Users\\hacer\\OneDrive\\Masaüstü\\IBM\\datasets\\'
-df_new.to_csv(path + "df.csv", index=False)
+    return df, dummy_variable.columns.values
