f26e9cf09eb28594e781b06a5b1522f84ccb3338
hacerakinci <108008177+hacerakinci@users.noreply.github.com>
2023-07-25 21:42:43 +0300

    Update Import_File.py

--- Import_File.py
+++ Import_File.py
@@ -1,10 +1,22 @@
 import os
 import pandas as pd
 import sys
-import shutil
+
+from tkinter import * 
+from tkinter import filedialog
+import tkinter as tk
+
+def browse_file():
+    root = tk.Tk()
+    root.withdraw()  # Hide the main tkinter window
+
+    file_path = filedialog.askopenfilename()
+    print(file_path)
+    return file_path
 
 #Hangi data formatında kontrol ediyor
-def check_data_format(file_path):
+def check_data_format():
+    file_path = browse_file()
     _, file_extension = os.path.splitext(file_path)
     
     # Creating empty dataFrame
@@ -25,4 +37,12 @@ def check_data_format(file_path):
         sys.exit()
 
     print("Dataset:", data_format)
-    return df
+    tempTuple = os.path.splitext(file_path)
+    file_path = tempTuple[0] + "_new" + file_extension 
+
+    print(file_path)
+
+    df.to_csv(file_path, index = False)
+    df = pd.read_csv( file_path, index_col = False)
+
+    return df, file_path
