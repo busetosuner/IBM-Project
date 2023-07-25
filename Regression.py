69d1b4558e41dc0c851c5d5f97326a96a5d61141
hacerakinci <108008177+hacerakinci@users.noreply.github.com>
2023-07-25 21:43:23 +0300

    Update Regression.py

--- Regression.py
+++ Regression.py
@@ -4,22 +4,18 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 import Numerical_Data
+import Binning
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-
-def perform_multiple_linear_regression(df, target_col, group_list, mp):
+def perform_multiple_linear_regression(df, target_col):
     # Convert the target variable to numeric if it is categorical
-    if not pd.api.types.is_numeric_dtype(df[target_col]):
-        df[target_col] = pd.factorize(df[target_col])[0]
+    if not Binning.is_numeric(df[target_col]):
+        df[target_col] = pd.factorize(target_col)[0]
     
-    # Use the dictionary "mp" to get the correct column names
-    feature_cols = [mp[col] if col in mp else col for col in group_list]
 
-    # Select only numeric columns for the feature variables
-
-    numeric_columns = Numerical_Data.numeric_columns(df)
-    
     # Split the data into features (X) and target (y)
-    X = df[numeric_columns]
+    X = df.drop(target_col, axis= 1)
     y = df[target_col]
 
     # Split the data into training and testing sets (80% training, 20% testing)
@@ -31,11 +27,23 @@ def perform_multiple_linear_regression(df, target_col, group_list, mp):
     
     # Predict the target variable on the test set
     y_pred = model.predict(X_test)
-    
+
     # Evaluate the model
     mse = mean_squared_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
-    
-    
+    # Print regression results
+    print("\nMultiple Linear Regression Results:")
+    print("Coefficients:", model.coef_)
+    print("Intercept:", model.intercept_)
+    print("Mean Squared Error (MSE):", mse)
+    print("R-squared (R2):", r2)
+
+    # Plot regression results
+    plt.figure(figsize=(8, 6))
+    sns.scatterplot(x=y_test, y=y_pred)
+    plt.xlabel('Actual Target Values')
+    plt.ylabel('Predicted Values')
+    plt.title('Regression Plot')
+    plt.show()
     
     return model, mse, r2, df
