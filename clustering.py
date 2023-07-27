a7e5b35266089747904563a07b06dcf34b83c5cd
Buse Tosuner <busetosuner@gmail.com>
2023-07-27 10:21:54 +0300

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -1,77 +1,79 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, AgglomerativeClustering
 from IPython.display import clear_output
-import handle_missing_values
 
-def plot_clusters(df, labels, centroids, iteration):
+def plot_clusters(df, labels, centroids=None, iteration=None):
+    if len(df.columns) <= 1:
+        print("Data has only one feature. Cannot perform PCA for 2 components.")
+        return
+
     pca = PCA(n_components=2)
     df_2d = pca.fit_transform(df)
-    centroids_2d = pca.fit_transform(centroids.T)
-    clear_output(wait = True)
-    plt.title(f'Iteration{iteration}')
-    plt.scatter(x=df_2d[:,0], y= df_2d[:,1], c = labels)
-    plt.scatter(x=centroids_2d[:,0], y=centroids_2d[:,1])
-    plt.pause(0.05)
+    if centroids is not None:
+        # Reshape centroids to 2D array if it's not already
+        centroids = centroids.values.reshape(1, -1)
+        centroids_2d = pca.transform(centroids)
+    clear_output(wait=True)
+    plt.title(f'Iteration {iteration}' if iteration else 'Final Result')
+    plt.scatter(x=df_2d[:, 0], y=df_2d[:, 1], c=labels)
+    if centroids is not None:
+        plt.scatter(x=centroids_2d[:, 0], y=centroids_2d[:, 1], marker='X', s=200, c='black')
+    plt.pause(0.5 if iteration else 3)
 
-def plot_clusters_scatter(df, labels, centroids, iteration):
-    centroid_T= centroids.T
-    plt.title(f'Iteration{iteration}')
-    plt.scatter(x=df.iloc[:,0], y= df.iloc[:,1], c = labels)
-    plt.scatter(x=centroid_T.iloc[:,0], y=centroid_T.iloc[:,1])
-    plt.pause(0.05)
 
-def random_centroids(df, k):
-    centroids = []
-    for i in range(k):
-        centroid = df.apply(lambda x: float(x.sample()))
-        centroids.append(centroid)
-    return pd.concat(centroids, axis=1)
+def get_labels(df, model):
+    if isinstance(model, KMeans):
+        return model.labels_
+    elif isinstance(model, AgglomerativeClustering):
+        return model.fit_predict(df)
+    else:
+        raise ValueError("Unknown model type!")
 
-def get_labels(df, centroids):
-    distances = centroids.apply(lambda x: np.sqrt(((df - x)**2).sum(axis=1)))
-    return distances.idxmin(axis=1)
-
-def new_centroids(df, labels, k):
-    np.seterr(divide = 'ignore')
-    centroids = df.groupby(labels).apply(lambda x: np.exp(np.log(x).mean())).T
-    return centroids
 
 def cluster(df, k):
-    X = df 
-    kmeans = KMeans(n_clusters=k, n_init='auto')
-    kmeans.fit(X)
+    if len(df.columns) <= 1:
+        print("Data has only one feature. Cannot perform clustering.")
+        return
 
-    identified_clusters = kmeans.fit_predict(X)
-    # print("Identified Clusters: ", identified_clusters)
+    # K-means clustering
+    kmeans = KMeans(n_clusters=k, n_init=10)  # Set the value of n_init explicitly
+    kmeans.fit(df)
+    kmeans_labels = get_labels(df, kmeans)
 
-    data_with_clusters = df.copy()
-    data_with_clusters['Clusters'] = identified_clusters
+    # Hierarchical Clustering (Agglomerative)
+    hierarchical = AgglomerativeClustering(n_clusters=k)
+    hierarchical_labels = get_labels(df, hierarchical)
 
-    centroids = random_centroids(df, k)
-    # print("Centroids: \n", centroids)
+    # Plot initial data
+    plot_clusters(df, kmeans_labels)
 
-    labels = get_labels(df, centroids)
-    # print("Labels counts: ", labels.value_counts())
-
-    old_centroids = pd.DataFrame()
-
+    # K-means clustering with animation
+    old_centroids = None
     max_iteration = 100
-    iteration = 1  
+    
 
-    while iteration < max_iteration and not centroids.equals(old_centroids):
+    for iteration in range(1, max_iteration + 1):
+        centroids = df.apply(lambda x: x.sample().iloc[0])
+        kmeans_labels = get_labels(df, kmeans)
+    
+        if centroids.equals(old_centroids):
+            break
         old_centroids = centroids
-        labels = get_labels(df, centroids)
-        centroids = new_centroids(df, labels, k)
-        centroids = handle_missing_values.clean_missing(centroids)
-        # print("Centroids: \n", centroids)
-        plot_clusters(df, labels, centroids, iteration)
-        iteration += 1
-    plt.show()
+        plot_clusters(df, kmeans_labels, centroids, iteration)
+    plot_clusters(df, kmeans_labels, centroids)
 
-"""   
-    plt.scatter(df.iloc[:,0], df.iloc[:,1], c=data_with_clusters['Clusters'], cmap='rainbow')
-    plt.show()    
- """
+    # Hierarchical Clustering (Agglomerative) with the final result
+    plot_clusters(df, hierarchical_labels)
+
+    # Compare the two algorithms and select the better one
+    kmeans_sse = kmeans.inertia_
+    hierarchical_sse = np.sum((df - np.array(old_centroids.T)) ** 2)  # Using the last K-means centroids as hierarchical centroids
+    print("K-Means SSE:", kmeans_sse)
+    print("Hierarchical Clustering SSE:", hierarchical_sse)
+
+    if kmeans_sse < hierarchical_sse.values[0]:  # Accessing the scalar value from the Series
+        print("The better algorithm is: K-Means")
+    else:
+        print("The better algorithm is: Hierarchical Clustering (Agglomerative)")
