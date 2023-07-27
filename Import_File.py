9a05c4ae2bdfa252a75f0537483406b1653ae735
Buse Tosuner <busetosuner@gmail.com>
2023-07-27 16:08:17 +0300

    Update Import_File.py

--- Import_File.py
+++ Import_File.py
@@ -1,3 +1,4 @@
+from tkinter import messagebox
 import os
 import pandas as pd
 import sys
@@ -6,17 +7,28 @@ from tkinter import *
 from tkinter import filedialog
 import tkinter as tk
 
-def browse_file():
-    root = tk.Tk()
+# Function to display the welcome message
+def welcome_message(root):
+    root.withdraw()  # Hide the main tkinter window
+    welcome_msg = "Welcome to Scourgify! Please select a dataset to proceed."
+    messagebox.showinfo("Welcome", welcome_msg)
+
+def browse_file(root):
+    
     root.withdraw()  # Hide the main tkinter window
 
     file_path = filedialog.askopenfilename()
+    # root.destroy()  # Destroy the extra window after file selection
     print(file_path)
     return file_path
 
-#Hangi data formatında kontrol ediyor
+
+
+# Hangi data formatında kontrol ediyor
 def check_data_format():
-    file_path = browse_file()
+    root = tk.Tk()
+    welcome_message(root)  # Display welcome message
+    file_path = browse_file(root)
     _, file_extension = os.path.splitext(file_path)
     
     # Creating empty dataFrame
@@ -42,7 +54,7 @@ def check_data_format():
 
     print(file_path)
 
-    df.to_csv(file_path, index = False)
-    df = pd.read_csv( file_path, index_col = False)
+    df.to_csv(file_path, index=False)
+    df = pd.read_csv(file_path, index_col=False)
 
     return df, file_path
