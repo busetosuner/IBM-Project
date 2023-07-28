11e3c0f0dc2bd33c64735a6232754b359bb2c477
Buse Tosuner <busetosuner@gmail.com>
2023-07-28 14:37:48 +0300

    Update EDA_ANOVA.py

--- EDA_ANOVA.py
+++ EDA_ANOVA.py
@@ -1,45 +1,60 @@
+import pandas as pd
+from scipy.stats import f_oneway
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# DEA  ----- Descriptive Statistics/// Group By /// ANOVA /// Correlation
+def perform_anova(df):
+    # Column bulma categorical için
+    categorical_columns = df.select_dtypes(include='object').columns
 
+    # Column bulma numerical data icin
+    numeric_columns = df.select_dtypes(include='number').columns
 
-# ANOVA
+    results = []
+    df_results = pd.DataFrame()  # Boş bir DataFrame oluşturuyoruz
 
-# Her bir kategorik satırdaki columnları karşılaştırdı ama kullanıcıya sorarak da ilerleyebiliriz hangi sütunları karşılaştırmak istiyorsun diye
+    for column in categorical_columns:
+        groups = df[column].unique()  # Columndaki grupları alma
 
-# Column bulma categorical için
-categorical_columns = df_new.select_dtypes(include='object').columns
+        group_data = [df[df[column] == group][numeric_columns] for group in groups]  # Sayısal değişkeni ve grupları kullanarak verileri oluşturma
 
-# Column bulma numerical data icin
-numeric_columns = df_new.select_dtypes(include='number').columns
+        stat, p_value = f_oneway(*group_data)  # Anova analizi
 
-for column in categorical_columns:
-    groups = df_new[column].unique() # Columndaki grupları alma
-    
-    # Gruplar arasında istatistiksel olarak anlamlı farklılık testi
-    
-    group_data = [df_new[df_new[column] == group][numeric_columns] for group in groups] # Sayısal değişkeni ve grupları kullanarak verileri oluşturma
-    
-    stat, p_value = f_oneway(*group_data) # Anova analizi
-    
-    print(f"Column: {column} | ANOVA Stats: {stat} | p-value: {p_value}")
+        result = {
+            'Column': column,
+            'ANOVA Stats': stat,
+            'p-value': p_value
+        }
 
+        results.append(result)
 
-# Görselleştirmek için çok seçenek var: Çubuk grafiği, kutu grafikleri vs. Çubuk kullandım
+    # Sonuçları DataFrame'e ekliyoruz
+    df_results = pd.concat([df_results, pd.DataFrame(results)], ignore_index=True)
 
+    # Filter out non-numeric rows from 'ANOVA Stats' column
+    numeric_mask = df_results['ANOVA Stats'].apply(lambda x: pd.api.types.is_numeric_dtype(x))
+    df_results = df_results[numeric_mask]
 
+    # Remove problematic rows in 'ANOVA Stats' column
+    df_results = df_results.dropna(subset=['ANOVA Stats'])
 
-for column in categorical_columns:
-    groups = df_new[column].unique()
-    anova_stats = []
-    
-    for group in groups:
-        data = df_new[df_new[column] == group][numeric_columns]
-        stat, p_value = f_oneway(*data.T.values)
-        anova_stats.append(stat)
-    
+    try:
+        # Convert the 'ANOVA Stats' column to numeric data type
+        df_results['ANOVA Stats'] = pd.to_numeric(df_results['ANOVA Stats'])
+    except Exception as e:
+        print("Error converting 'ANOVA Stats' column to numeric data type:")
+        print(e)
+
+    # Convert the 'Column' column to categorical data type
+    df_results['Column'] = pd.Categorical(df_results['Column'])
+
+    # Görselleştirme için
     plt.figure(figsize=(8, 6))
-    sns.barplot(x=groups, y=anova_stats)
-    plt.xlabel(column)
+    sns.barplot(x='Column', y='ANOVA Stats', data=df_results)
+    plt.xlabel('Column')
     plt.ylabel('ANOVA Stats')
-    plt.title(f'ANOVA: {column}')
+    plt.title('ANOVA Results')
+    plt.xticks(rotation=45)
     plt.show()
+
+    return results
