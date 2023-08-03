42bf539811394b28de1d0bf5993986a7834fbc6d
hacerakinci <108008177+hacerakinci@users.noreply.github.com>
2023-08-03 11:57:08 +0300

    Update Correlation.py

--- EDA/Correlation.py
+++ EDA/Correlation.py
@@ -5,10 +5,7 @@ def calculate_correlation(df, target):
     
     numeric_columns = df.select_dtypes(include='number').columns
 
-    
     correlation_matrix = df[numeric_columns].corr()
-
-    
     target_correlation = correlation_matrix[target]
     
     #print("\nTarget ({}):".format(target))
@@ -16,14 +13,10 @@ def calculate_correlation(df, target):
 
     plt.figure(figsize=(13, 10))
     sns.heatmap(correlation_matrix, annot=False, cmap='coolwarm')
-    plt.title('Korelasyon Matrisi')
+    plt.title('Correlation Matrix')
     plt.show()  
 
     highly_corr = df.columns[abs(target_correlation) >= 0.5].tolist()
 
     print("This attiributes are highly correlated with target: \n",highly_corr)
     return target_correlation
-    
-    
-
-                    
